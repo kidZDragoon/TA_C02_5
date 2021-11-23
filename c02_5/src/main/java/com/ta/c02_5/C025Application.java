1dff3d48685064991a2441f3c2480c3062e08ca2
Farhan Ahmad Primaditya <farhan.primaditya@gmail.com>
2021-11-23 20:39:00 +0700

    feat: initialize spring boot project

---
+++ c02_5/src/main/java/com/ta/c02_5/C025Application.java
@@ -0,0 +1,13 @@
+package com.ta.c02_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C025Application.class, args);
+	}
+
+}
---
+++ c02_5/src/test/java/com/ta/c02_5/C025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ta.c02_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
