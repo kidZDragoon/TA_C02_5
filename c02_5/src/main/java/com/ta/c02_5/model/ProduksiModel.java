26fbc325771225eee34ac2b58bd466268a008a43
tsanaa patricia <patriciakhonsaa@gmail.com>
2021-11-26 22:09:06 +0700

    feat: implement fitur login & logout

--- c02_5/src/main/java/com/ta/c02_5/model/ProduksiModel.java
+++ c02_5/src/main/java/com/ta/c02_5/model/ProduksiModel.java
@@ -47,21 +47,21 @@ public class ProduksiModel implements Serializable {
 
     // Relasi dengan PegawaiModel
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_pegawai", referencedColumnName = "idPegawai")
+    @JoinColumn(name = "id_pegawai", referencedColumnName = "id_pegawai")
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private PegawaiModel pegawai;
 
     // Relasi dengan RequestUpdateItemModel
     @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_request_update_item", referencedColumnName = "idRequestUpdateItem")
+    @JoinColumn(name = "id_request_update_item", referencedColumnName = "id_request_update_item")
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private RequestUpdateItemModel requestUpdateItem;
 
     // Relasi dengan MesinModel
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "id_mesin", referencedColumnName = "idMesin")
+    @JoinColumn(name = "id_mesin", referencedColumnName = "id_mesin")
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private MesinModel mesin;
