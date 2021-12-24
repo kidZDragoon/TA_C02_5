66cfa02ddf3260612f1cef1f44e0159ffb4aab33
Ahmad Imam Fauzan <89480136+ahmadimamfauzan@users.noreply.github.com>
2021-12-24 16:44:44 +0700

    Feat/auth (#39)
    * feat: fix fitur 4
    
    * feat: fix pegawai counter fitur 4
    
    * feat: add authentication on fitur 13 & 14
    
    * feat: add auth on fitur 13 14
    
    * feat: fix auth
    
    * feat: fix auth
    
    * fix
    
    Co-authored-by: ahmadimamfauzan <ahmadimamfauzan@gmail.com>

--- c02_5/src/main/java/com/ta/c02_5/service/ItemRestService.java
+++ c02_5/src/main/java/com/ta/c02_5/service/ItemRestService.java
@@ -21,4 +21,6 @@ public interface ItemRestService {
 
     List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
 
+    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
+
 }
