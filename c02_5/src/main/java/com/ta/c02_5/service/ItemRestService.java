e28811a277e38cb6a275951c750ca136cbcbaf72
Ahmad Imam Fauzan <89480136+ahmadimamfauzan@users.noreply.github.com>
2021-12-24 20:56:00 +0700

    feat: change all files to zahra's last commit (#41)
    Co-authored-by: ahmadimamfauzan <ahmadimamfauzan@gmail.com>

--- c02_5/src/main/java/com/ta/c02_5/service/ItemRestService.java
+++ c02_5/src/main/java/com/ta/c02_5/service/ItemRestService.java
@@ -13,35 +13,4 @@ public interface ItemRestService {
     Mono<HashMap> updateStokItem (ItemDetail item);
     List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
-
-<<<<<<< HEAD
-    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
-
-<<<<<<< HEAD
-    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
-
-<<<<<<< HEAD
-=======
-    List<HashMap<String, Object>> getProposedItemHashMap(ItemModel proposedItem);
-
->>>>>>> parent of 02fed9f (Feat/auth (#40))
-=======
->>>>>>> parent of 7251d35 (Feat/auth (#38))
-=======
->>>>>>> parent of 7251d35 (Feat/auth (#38))
-=======
->>>>>>> parent of f88f595 (Feat/auth (#37))
-=======
->>>>>>> parent of 575c8b9 (Feat/auth (#36))
-=======
->>>>>>> parent of b3ec948 (Feat/auth (#35))
-=======
->>>>>>> parent of b3ec948 (Feat/auth (#35))
 }
